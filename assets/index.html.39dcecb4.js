1adce11881f907ec44940f75ed88b6cef24387fe
Mister-Hope <mister-hope@outlook.com>
2022-07-18 14:51:16 +0000

    Deploying to gh-pages from @ vuepress-theme-hope/vuepress-theme-hope@97e916598a0c6d30ab01893146fb54190c6f58d7 🚀

---
+++ assets/index.html.39dcecb4.js
@@ -0,0 +1,12 @@
+import{_ as n,o as s,c as a,a as e}from"./app.de77a4bf.js";const t={},i=e(`<h2 id="demo" tabindex="-1"><a class="header-anchor" href="#demo" aria-hidden="true">#</a> Demo</h2><div class="language-javascript ext-js line-numbers-mode"><pre class="language-javascript"><code><span class="token comment">// .vuepress/config.js</span>
+module<span class="token punctuation">.</span>exports <span class="token operator">=</span> <span class="token punctuation">{</span>
+  <span class="token literal-property property">plugins</span><span class="token operator">:</span> <span class="token punctuation">[</span>
+    <span class="token punctuation">[</span>
+      <span class="token string">&quot;copy-code2&quot;</span><span class="token punctuation">,</span>
+      <span class="token punctuation">{</span>
+        <span class="token comment">// options</span>
+      <span class="token punctuation">}</span><span class="token punctuation">,</span>
+    <span class="token punctuation">]</span><span class="token punctuation">,</span>
+  <span class="token punctuation">]</span><span class="token punctuation">,</span>
+<span class="token punctuation">}</span><span class="token punctuation">;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),c=[i];function o(p,l){return s(),a("div",null,c)}var d=n(t,[["render",o],["__file","index.html.vue"]]);export{d as default};
